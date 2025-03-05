dbcef1e4773c8f1f11a31dfef30f8a43e382118d
rhuan-rgb <rhuan.lima.cmd@gmail.com>
2025-03-05 13:43:13 -0300

    estilização masomeno

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,10 @@ import {Link} from "react-router-dom"
 function Home(){
     return (
         <div style={{display:'flex', flexDirection:"column", alignItems:"center", justifyContent:'space-between'}}>
-            <h1>Bem-vindo ao sistema de eventos!</h1>
-            <Link to="eventos">Eventos</Link>
-            <Link to="ingressos">Ingressos</Link>
-            <Link to="organizadores">Organizadores</Link>
+            <h1 style={{backgroundColor:'#19b6e8', borderRadius:"5px"}}>Bem-vindo ao sistema de eventos!</h1>
+            <Link to="eventos" style={{fontSize:'20px', marginBottom:"10px"}}>Eventos</Link>
+            <Link to="ingressos" style={{fontSize:'20px', marginBottom:"10px"}}>Ingressos</Link>
+            <Link to="organizadores" style={{fontSize:'20px', marginBottom:"10px"}}>Organizadores</Link>
         </div>
     )
 }
